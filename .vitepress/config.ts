f0b0f59a8667652fb7226af85aa4e1b3c08a872c
Jose Pereira <josebatistapereira@gmail.com>
2026-07-26 16:16:50 -0300

    organize project

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: 'josebatista.dev',
   description: 'Linux OS Portfolio',
-  srcDir: '.',
+  srcDir: 'src',
   cleanUrls: true,
   appearance: false,
   themeConfig: {}
