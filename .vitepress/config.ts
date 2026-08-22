14c35bf3af899505b9b769247b61533afe43382b
Jose Pereira <josebatistapereira@gmail.com>
2026-08-22 12:31:54 -0300

    add sitemap config and robots.txt/llms.txt

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -9,6 +9,9 @@ export default defineConfig({
   markdown: {
     theme: 'github-dark',
   },
+  sitemap: {
+    hostname: 'https://josebatista.dev',
+  },
   head: [
     ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200' }]
   ],
